23e3a3f3c6e5b96becf43d4a4dddbe88f4973bc1
Navila48 <Navila48@users.noreply.github.com>
2021-07-10 12:13:01 +0600

    134. Gas Station

---
+++ GreedyAlgo/134. Gas Station.java
@@ -0,0 +1,40 @@
+Problem:
+134. Gas Station
+
+There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+Example 1:
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+Output: 3
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.
+
+Hint:
+Possible two cases.
+case1: If the total amount of gas is less than total cose then simply return -1;That means it not possible to travel
+Case2:Start from the gas station where amount of gas is higher than cost or required gas.
+
+Solution:
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total=0,start=0,tank=0;
+        int n=gas.length;
+        for(int i=0;i<n;i++){
+            tank+=(gas[i]-cost[i]);
+            total+=(gas[i]-cost[i]);
+            if(tank<0){
+                start=(i+1)%n;
+                tank=0;
+            }
+        }
+       return total<0?-1:start;
+        
+    }
+}
